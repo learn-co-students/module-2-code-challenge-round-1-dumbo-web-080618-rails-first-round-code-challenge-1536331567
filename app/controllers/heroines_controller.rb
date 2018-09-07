93b9e38b075b59a18bbf6bc7082bb99cca364a32
haleybarlar <hbarlar1@gmail.com>
2018-09-07 11:54:22 -0400

    Done.

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,46 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def new
+    @heroine = Heroine.new
+    render :new
+  end
+
+  def create
+    @heroine = Heroine.create(heroine_params)
+    if @heroine.valid?
+      redirect_to @heroine
+    else
+      render :new
+    end
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+    render :show
+  end
+
+  def edit
+    @heroine = Heroine.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @heroine = Heroine.find(params[:id])
+    @heroine.update(heroine_params)
+    redirect_to @heroine
+  end
+
+  def destroy
+    @heroine = Heroine.find(params[:id])
+    @heroine.destroy
+    redirect_to @heroines
+  end
+
+  private
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
 end
